83409536eeeef567de52ef1b3fb667b8140bfdd8
Jie018 <cynthia381@gmail.com>
2024-05-22 17:45:09 +0800

    v2_游欣婕

--- app/src/main/java/pu/edu/tw/yu/s1114629/SecondActivity.kt
+++ app/src/main/java/pu/edu/tw/yu/s1114629/SecondActivity.kt
@@ -1,34 +1,21 @@
 package pu.edu.tw.yu.s1114629
 
-import android.content.Intent
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -36,30 +23,29 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import pu.edu.tw.yu.s1114629.ui.theme.S1114629Theme
 
-class MainActivity : ComponentActivity() {
+class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             S1114629Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("簡介")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting2("主要結構")
                 }
             }
         }
     }
 }
-@OptIn(ExperimentalMaterial3Api::class)
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
     val context = LocalContext.current
+    val activity = (context as Activity)
     Row {
         Image(
             painter = painterResource(id = R.drawable.maria),
@@ -72,17 +58,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(
             text = "$name",
             fontSize = 25.sp,
-            color = Color.Blue,
+            color = Color.Red,
             modifier = modifier
         )
-        Button(onClick = {
-            var it = Intent(context, SecondActivity::class.java)
-            context.startActivity(it)
-        })
+        Button(
+            onClick = {
+                activity.finish()
+            })
         {
-        Text(text = "回到MainActivity")
+            Text(text = "回到MainActivity")
         }
     }
 }
 
-
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    S1114629Theme {
+        Greeting2("Android")
+    }
+}
